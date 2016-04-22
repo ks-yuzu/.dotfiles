83549cab1a92721d46a1fad4bfbfbe6910161b5e
Oosako Yuuki <yuuki.oosako@kwansei.ac.jp>
2016-04-22 11:32:19 +0900

    Updated link-gen.sh for .Xresources and .Xdefaults

--- symbolic-link-gen.sh
+++ symbolic-link-gen.sh
@@ -1,4 +1,11 @@
 #!/usr/bin/zsh
 
-sudo ln -s ~/.dotfiles/.tmux.conf ~/.tmux.conf
-sudo ln -s ~/.dotfiles/.peco      ~/.peco
+touch ~/.zshrc
+echo 'source ~/.dotfiles/zsh/init.zsh' > ~/.zshrc
+
+sudo ln -s ~/.dotfiles/xmonad/          ~/.xmonad
+sudo ln -s ~/.dotfiles/xmonad/.xmobarrc ~/.xmobarrc
+sudo ln -s ~/.dotfiles/.tmux.conf       ~/.tmux.conf
+sudo ln -s ~/.dotfiles/.peco            ~/.peco
+sudo ln -s ~/.dotfiles/.Xresources      ~/.Xresources
+sudo ln -s ~/.dotfiles/.Xdefaults       ~/.Xdefaults
