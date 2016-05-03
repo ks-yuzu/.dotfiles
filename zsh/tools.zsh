723dedcb924036ca95ee32bea2cf392f46327d06
Yuuki Oosako <yuuki.oosako@kwansei.ac.jp>
2016-05-03 20:37:58 +0900

    Fixed function of compilding tex

--- zsh/tools.zsh
+++ zsh/tools.zsh
@@ -149,25 +149,29 @@ function texcompile()
     basename=$1                 # tex-file name
     filename=${basename%.*}     # no suffix
 
-    if [ $(file $basename | grep UTF-8 | wc -l) -lt 1 ]; then
-        echo '[   convert to UTF-8   ]'
-        nkf -w --overwrite $basename
-    else
-        echo '[ check encode (UTF-8) ]'
-    fi
+    export TEXINPUTS=$TEXINPUTS:./:~/works/styles
 
     if [ -f ${filename}.tex ]; then
-        if [ -f /tmp/$filename.tex ] && rm -f /tmp/${filename}.tex
+        if [ $(file $basename | grep UTF-8 | wc -l) -lt 1 ]; then
+            echo '[ convert to UTF-8     ]'
+            nkf -w --overwrite $basename
+        else
+            echo '[ check encode (UTF-8) ]'
+        fi
+
+        if [ -f /tmp/$filename.tex ]; then
+            rm -f /tmp/${filename}.tex
+        fi
         cp ${filename}.tex /tmp
-        pushd > /dev/null
+        pushd . > /dev/null
         cd /tmp > /dev/null
-        platex ${filename}.tex  > /dev/null     # tex -> dvi
         echo '[ generate tex -> dvi  ]'
+        platex ${filename}.tex      # tex -> dvi
     else
         echo "file : '${filename}.tex' does not exist."
         return
     fi
- 
+
     if [ -f ${filename}.dvi ]; then
         dvipdfmx ${filename}.dvi 2> /dev/null   # dvi -> pdf
         echo '[ generate dvi -> pdf  ]'
@@ -175,14 +179,14 @@ function texcompile()
         echo "file : '/tmp/${filename}.dvi' does not exist."
         return
     fi
- 
+
     if [ -f ${filename}.pdf ]; then
         popd > /dev/null 2>&1
         rm -f ${filename}.pdf 2> /dev/null
         cp /tmp/${filename}.pdf ./
-        gnome-open ${filename}.pdf > /dev/null 2>&1 # open pdf
-        echo '[  open generated pdf  ]'
+        # echo '[ open generated pdf   ]'
+        # evince ${filename}.pdf &  # open pdf
     else
         echo "file : '/tmp/${filename}.pdf' does not exist."
-    fi
+    fi  
 }
