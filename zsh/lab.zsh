47e362beff3cbb543d0fb20274ca678c086e94a6
Yuuki Oosako <yuuki.oosako@kwansei.ac.jp>
2016-05-10 17:08:40 +0900

    Fixed nas-mount-scripts in zsh/lab.zsh

--- zsh/lab.zsh
+++ zsh/lab.zsh
@@ -12,7 +12,7 @@ function inas()
 	if [ $(mount -v | grep $LAB_NAS_MOUNT_POINT_LOCAL | wc -l)  -eq '0' ];then
         sudo mkdir $LAB_NAS_MOUNT_POINT_LOCAL
 		sudo mount -t cifs //192.168.0.16/disk1 $LAB_NAS_MOUNT_POINT_LOCAL \
-			 -o iocharset=utf8,username=root,password=snoopy2015,file_mode=0755,dir_mode=0755,uid=$(id -u),gid=$(id -g)
+			 -o iocharset=utf8,username=user,password=pass,file_mode=0755,dir_mode=0755,uid=$(id -u),gid=$(id -g)
 	fi
 	cd $LAB_NAS_MOUNT_POINT_LOCAL
 }
@@ -33,15 +33,18 @@ function uinas()
 ##### scripts for sshfs #####
 
 export LAB_NAS_MOUNT_POINT_SSH='/mnt/ssh-labnas'
+export LAB_SERVER10_KEY_PATH='/home/yuzu/.ssh/ex_computer/id_ed25519_excomputer'
+#export LAB_SERVER_IP='192.218.172.58'
+export LAB_SERVER_IP='192.168.0.10'
 
 function sshinas()
 {
     if [ $(mount -v | grep $LAB_NAS_MOUNT_POINT_SSH | wc -l)  -eq '0' ];then
         sudo mkdir $LAB_NAS_MOUNT_POINT_SSH
         sudo sshfs \
-             yuzu@192.168.0.10:$LAB_NAS_MOUNT_POINT_LOCAL \
+             yuzu@$LAB_SERVER_IP:$LAB_NAS_MOUNT_POINT_LOCAL \
              $LAB_NAS_MOUNT_POINT_SSH \
-             -o uid=$(id -u),gid=$(id -g),allow_other,IdentityFile=/home/yuzu/.ssh/ex_computer/id_ed25519_excomputer
+             -o uid=$(id -u),gid=$(id -g),allow_other,IdentityFile=$LAB_SERVER10_KEY_PATH
     fi
     cd /mnt/ssh-labnas
 }
