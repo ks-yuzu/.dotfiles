fe2ec7c31b57534800cb167e982d0cd1f8f4b81c
Yuuki Oosako <yuuki.oosako@kwansei.ac.jp>
2016-05-10 17:26:42 +0900

    Added dir check to zsh/lab.zsh (and little fix)

--- zsh/lab.zsh
+++ zsh/lab.zsh
@@ -25,8 +25,9 @@ function uinas()
 
     if [ $(mount -v | grep $LAB_NAS_MOUNT_POINT_LOCAL | wc -l)  -ge '1' ];then
 	    sudo umount $LAB_NAS_MOUNT_POINT_LOCAL
-        sudo rmdir $LAB_NAS_MOUNT_POINT_LOCAL
     fi
+
+    if [ -d $LAB_NAS_MOUNT_POINT_LOCAL ] && sudo rmdir $LAB_NAS_MOUNT_POINT_LOCAL
 }
 
 
@@ -34,19 +35,20 @@ function uinas()
 
 export LAB_NAS_MOUNT_POINT_SSH='/mnt/ssh-labnas'
 export LAB_SERVER10_KEY_PATH='/home/yuzu/.ssh/ex_computer/id_ed25519_excomputer'
-#export LAB_SERVER_IP='192.218.172.58'
-export LAB_SERVER_IP='192.168.0.10'
+export LAB_SERVER_IP='192.218.172.58'
+#export LAB_SERVER_IP='192.168.0.10'
 
 function sshinas()
 {
+    if [ ! -d $LAB_NAS_MOUNT_POINT_SSH ] && sudo mkdir $LAB_NAS_MOUNT_POINT_SSH
+    
     if [ $(mount -v | grep $LAB_NAS_MOUNT_POINT_SSH | wc -l)  -eq '0' ];then
-        sudo mkdir $LAB_NAS_MOUNT_POINT_SSH
         sudo sshfs \
              yuzu@$LAB_SERVER_IP:$LAB_NAS_MOUNT_POINT_LOCAL \
              $LAB_NAS_MOUNT_POINT_SSH \
              -o uid=$(id -u),gid=$(id -g),allow_other,IdentityFile=$LAB_SERVER10_KEY_PATH
     fi
-    cd /mnt/ssh-labnas
+    cd $LAB_NAS_MOUNT_POINT_SSH
 }
 
 function usshinas()
@@ -54,8 +56,10 @@ function usshinas()
 	if [ $(pwd | grep $LAB_NAS_MOUNT_POINT_SSH | wc -l)  -eq '1' ];then
 		cd > /dev/null
 	fi
+
     if [ $(mount -v | grep $LAB_NAS_MOUNT_POINT_LOCAL | wc -l)  -ge '1' ];then
 	    sudo umount $LAB_NAS_MOUNT_POINT_SSH
-        sudo rmdir $LAB_NAS_MOUNT_POINT_SSH
     fi
+
+    if [ -d $LAB_NAS_MOUNT_POINT_SSH ] && sudo rmdir $LAB_NAS_MOUNT_POINT_SSH
 }
