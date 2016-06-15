95275a32735e199fa7dee9afa15f0b59bcce8a2e
Oosako Yuuki <yuuki.oosako@kwansei.ac.jp>
2016-06-15 12:55:52 +0900

    Change to start up tmux automatically only in ssh

--- zsh/startup.zsh
+++ zsh/startup.zsh
@@ -1,7 +1,22 @@
-## tmux自動起動
-#Start tmux on every shell login
-#https://wiki.archlinux.org/index.php/Tmux#Start_tmux_on_every_shell_login
-if which tmux 2>&1 >/dev/null; then
-    #if not inside a tmux session, and if no session is started, start a new session
- #   test -z "$TMUX" && (tmux attach || tmux new-session)
+## Start tmux automatically on ssh shell
+# https://gist.github.com/ABCanG/11bfcff22a0633600aefbb01550b8e38
+
+if [[ -n "${REMOTEHOST}${SSH_CONNECTION}" && -z "$TMUX" && -z "$STY" ]] && type tmux >/dev/null 2>&1; then
+    function confirm {
+        MSG=$1
+        while :
+        do
+            echo -n "${MSG} [Y/N]: "
+            read ans
+            case $ans in
+                [yY]) return 0 ;;
+                [nN]) return 1 ;;
+            esac
+        done
+    }
+    option=""
+    if tmux has-session && tmux list-sessions; then
+        option="attach"
+    fi
+    tmux $option && confirm "exit?" && exit
 fi
