6da4d1382ee61783f7969ae7ba1965bb76a98307
Yuuki Oosako <yuuki.oosako@kwansei.ac.jp>
2017-02-07 14:29:27 +0900

    Update zsh/other.zsh

--- zsh/others.zsh
+++ zsh/others.zsh
@@ -3,3 +3,7 @@ export EDITOR='emacsclient -t'
 export CVSEDITOR="${EDITOR}"
 export SVN_EDITOR="${EDITOR}"
 export GIT_EDITOR="${EDITOR}"
+
+# less command
+export VISUAL='emacsclient -t'
+export LESS='-N -M -R'
