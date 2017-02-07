fe5ee25d20e80cd2405ed256e6428c0c4740525c
Yuuki Oosako <yuuki.oosako@kwansei.ac.jp>
2017-02-07 14:30:42 +0900

    Add zsh/hls.zsh

---
+++ zsh/hls.zsh
@@ -0,0 +1,29 @@
+alias acap='acap.pl -Dall --remain-linked-object -l synthesized/'
+
+alias ise='ise NL'
+alias vivado="${HOME}/bin/Xilinx/Vivado/2016.4/bin/vivado"
+
+
+function clean-acap {
+    local dir='.'
+    if [ -d synthesized ]; then
+        dir='synthesized'
+    fi
+    
+    rm -f $dir/*.o
+    rm -f $dir/*.s
+    rm -f $dir/*.0
+    rm -f $dir/user.obj
+    rm -f $dir/*.out
+    rm -f $dir/*.dmem
+    rm -f $dir/*.txt
+    rm -f $dir/*_i.mem
+    rm -f $dir/*_d.mem
+    rm -f $dir/*.low
+    rm -f $dir/*.dfa
+    rm -f $dir/*.sch
+    rm -f $dir/*.bnd
+    rm -f $dir/*.stl
+    rm -f $dir/*.rtl
+    rm -f $dir/*.v
+}
