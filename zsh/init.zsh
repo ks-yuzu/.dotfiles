ba56f3a50db03b4deca90c8737cebb3c1c0f0979
Yuuki Oosako <yuuki.oosako@kwansei.ac.jp>
2017-05-12 13:30:53 +0900

    Update init.zsh

--- zsh/init.zsh
+++ zsh/init.zsh
@@ -3,37 +3,37 @@
 autoload -Uz compinit
 compinit -u
 
-setopt extended_glob			# use wildcard
+setopt extended_glob                         # use wildcard
 zstyle ':completion:*:default' menu select=2 # cursor select completion
 
-setopt auto_param_keys			# completion of a pair of () {} etc.
+setopt auto_param_keys                       # completion of a pair of () {} etc.
 setopt auto_param_slash
 
-setopt correct					# spell check
-setopt brace_ccl				# {a-c} -> a b c
-
+setopt correct                               # spell check
+setopt brace_ccl                             # {a-c} -> a b c
 
 
 #### [ key ]
-if [ $(type setxkbmap | grep 'setxkbmap is' | wc -l) -ge 1 ]; then
-    setxkbmap -option ctrl:nocaps    # caps lock -> ctrl
-else
-    echo '"setxkbmap" not fount'
+bindkey -e                                   # emacs like
+bindkey "\e[Z" reverse-menu-complete         # enable Shift-Tab
+
+setopt ignore_eof                            # Ctrl+Dでzshを終了しない
+
+# CapsLock -> ctrl
+if (( ${+commands[setxkbmap]} )); then
+    setxkbmap -option ctrl:nocaps
 fi
 
-setopt ignore_eof				     # Ctrl+Dでzshを終了しない
-
-bindkey -e                           # emacs like
-bindkey "\e[Z" reverse-menu-complete # enable Shift-Tab
+# xmodmap
+[ -f ~/.Xmodmap ] && xmodmap ~/.Xmodmap
 
 
-#### [ coloring ]
+
+#### [ color ]
 eval $(dircolors ~/.dotfiles/zsh/dircolors.256dark) # ls color
 
 # completion coloring
-if [ -n "$LS_COLORS" ]; then
-    zstyle ':completion:*' list-colors ${(s.:.)LS_COLORS}
-fi
+[ -n "$LS_COLORS" ] && zstyle ':completion:*' list-colors ${(s.:.)LS_COLORS}
 
 
 #### [ history ]
@@ -42,12 +42,12 @@ HISTSIZE=1000000
 SAVEHIST=1000000
 
 # Screenでのコマンド共有用
-setopt inc_append_history		# シェルを横断して.zsh_historyに記録
-setopt share_history			# ヒストリを共有
+setopt inc_append_history       # シェルを横断して.zsh_historyに記録
+setopt share_history            # ヒストリを共有
 
-setopt hist_ignore_dups			# 直前と同じコマンドをヒストリに追加しない
-setopt hist_ignore_all_dups		# ヒストリに追加されるコマンド行が古いものと同じなら古いものを削除
-setopt hist_ignore_space		# コマンドがスペースで始まる場合、コマンド履歴に追加しない
+setopt hist_ignore_dups         # 直前と同じコマンドをヒストリに追加しない
+setopt hist_ignore_all_dups     # ヒストリに追加されるコマンド行が古いものと同じなら古いものを削除
+setopt hist_ignore_space        # コマンドがスペースで始まる場合、コマンド履歴に追加しない
 
 
 #### [ others ]
