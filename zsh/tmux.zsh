eeaff8fe96f0246495aed25993c2e0959088771e
Yuuki Oosako <yuuki.oosako@kwansei.ac.jp>
2017-07-05 00:18:00 +0900

    Update the setting file to set the title of tmux window (title.zsh -> tmux.zsh)

---
+++ zsh/tmux.zsh
@@ -0,0 +1,9 @@
+autoload -Uz add-zsh-hook
+
+function rename_tmux_window() {
+    if [ $(echo $TERM | grep 'screen') ]; then
+        tmux rename-window $(pwd)
+    fi
+}
+
+add-zsh-hook precmd rename_tmux_window
