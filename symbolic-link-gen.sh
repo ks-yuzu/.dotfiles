4a7bb0d9421096076aeac471268fcfc57365469f
Yuuki Oosako <yuki.osako@gree.net>
2018-08-03 09:32:41 +0900

    Update symbolic-link-gen.sh

--- symbolic-link-gen.sh
+++ symbolic-link-gen.sh
@@ -10,3 +10,11 @@ sudo ln -s ~/.dotfiles/.peco            ~/.peco
 sudo ln -s ~/.dotfiles/.Xresources      ~/.Xresources
 sudo ln -s ~/.dotfiles/.Xdefaults       ~/.Xdefaults
 sudo ln -s ~/.dotfiles/.gitconfig       ~/.gitconfig
+
+## Mac home
+# .chunkwm_plugins            -> .dotfiles/chunkwm/.chunkwm_plugins/
+# .chunkwmrc                  -> .dotfiles/chunkwm/.chunkwmrc
+# .khdrc                      -> .dotfiles/khd/.khdrc
+# .tmux.conf                  -> .dotfiles/.tmux.conf.mac
+# com.googlecode.iterm2.plist -> .dotfiles/com.googlecode.iterm2.plis
+# .hammaerspoon               -> .dotfiles/
