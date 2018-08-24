4536d66fee0cbc0a15a5cf8d7217f905f9b79172
Yuuki Oosako <yuuki.oosako@kwansei.ac.jp>
2018-08-24 22:13:53 +0900

    Update zsh/path.zsh to disable environment variable 'PERL_LOCAL_LIB'

--- zsh/path.zsh
+++ zsh/path.zsh
@@ -1,11 +1,11 @@
 ## plenv
-PERL_LOCAL_LIB="$HOME/perl5"
-export PATH=${PERL_LOCAL_LIB}/bin:$PATH
-export PERL5LIB=${PERL_LOCAL_LIB}/lib/perl5:$PERL5LIB
+# PERL_LOCAL_LIB="$HOME/perl5"
+# export PATH=${PERL_LOCAL_LIB}/bin:$PATH
+# export PERL5LIB=${PERL_LOCAL_LIB}/lib/perl5:$PERL5LIB
 
 if [ -x "`which plenv`" ]; then
   export PLENV_VERSION=$(plenv version | awk '{print $1}')
-  export PERL_CPANM_OPT="--local-lib=${PERL_LOCAL_LIB}"
+  # export PERL_CPANM_OPT="--local-lib=${PERL_LOCAL_LIB}"
   export PATH=${PLENV_ROOT}/bin:$PATH
   export PERL5LIB=${PLENV_ROOT}/versions/${PLENV_VERSION}/lib/perl5:$PERL5LIB
 fi
