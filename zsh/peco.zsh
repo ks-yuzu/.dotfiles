dbf9f8c66b0fde5bd465dd43beaa09205b83b920
Yuki Osako <yuki.osako@G-PC-00561164.intra.gree-office.net>
2019-05-13 09:26:50 +0900

    Update peco-ssh to use ~/bin/peco-ssh.pl

--- zsh/peco.zsh
+++ zsh/peco.zsh
@@ -199,20 +199,27 @@ function rmpeco-rf()
 
 
 function peco-ssh () {
-    local selected_host=$(awk '
-  tolower($1)=="host" {
-    for (i=2; i<=NF; i++) {
-      if ($i !~ "[*?]") {
-        print $i
-      }
-    }
-  }
-  ' ~/.ssh/config | sort | peco --query "$LBUFFER")
-    if [ -n "$selected_host" ]; then
-        BUFFER="ssh ${selected_host}"
-        zle accept-line
-    fi
-    zle clear-screen
+  #   local selected_host=$(awk '
+  # tolower($1)=="host" {
+  #   for (i=2; i<=NF; i++) {
+  #     if ($i !~ "[*?]") {
+  #       print $i
+  #     }
+  #   }
+  # }
+  # ' ~/.ssh/config | sort | peco --query "$LBUFFER")
+  #   if [ -n "$selected_host" ]; then
+  #       BUFFER="ssh ${selected_host}"
+  #       zle accept-line
+  #   fi
+  #   zle clear-screen
+
+  local selected_host=$(perl ~/bin/peco-ssh.pl)
+  if [ -n "$selected_host" ]; then
+    BUFFER="ssh ${selected_host}"
+    zle accept-line
+  fi
+  zle clear-screen
 }
 zle -N peco-ssh
 bindkey '^\' peco-ssh
