b3cb7c9d3dd12a86f89d03ed1f4e130d247b2988
yuki.osako <yuki.osako@gree.net>
2020-02-03 18:32:32 +0900

    Update symbolic-link-gen.sh

--- symbolic-link-gen.sh
+++ symbolic-link-gen.sh
@@ -13,16 +13,19 @@ ln -s ~/.dotfiles/.gitconfig       ~/.gitconfig
 ln -s ~/.dotfiles/.gitattributes_global ~/.gitattributes_global
 
 ## X
-ln -s ~/.dotfiles/xmonad/          ~/.xmonad
-ln -s ~/.dotfiles/xmonad/.xmobarrc ~/.xmobarrc
-ln -s ~/.dotfiles/.Xresources      ~/.Xresources
-ln -s ~/.dotfiles/.Xdefaults       ~/.Xdefaults
+#ln -s ~/.dotfiles/xmonad/          ~/.xmonad
+#ln -s ~/.dotfiles/xmonad/.xmobarrc ~/.xmobarrc
+#ln -s ~/.dotfiles/.Xresources      ~/.Xresources
+#ln -s ~/.dotfiles/.Xdefaults       ~/.Xdefaults
+
+## skk jisyo
+ln -s ~/.dotfiles/SKK-JISYO.L.utf8 ~/.SKK-JISYO.L.utf8
 
 
 ## Mac home
-# .chunkwm_plugins            -> .dotfiles/chunkwm/.chunkwm_plugins/
-# .chunkwmrc                  -> .dotfiles/chunkwm/.chunkwmrc
-# .khdrc                      -> .dotfiles/khd/.khdrc
-# .tmux.conf                  -> .dotfiles/.tmux.conf.mac
-# com.googlecode.iterm2.plist -> .dotfiles/com.googlecode.iterm2.plis
-# .hammaerspoon               -> .dotfiles/
+ln -s ~/.dotfiles/chunkwm/.chunkwm_plugins/    ~/.chunkwm_plugins           
+ln -s ~/.dotfiles/chunkwm/.chunkwmrc           ~/.chunkwmrc                 
+ln -s ~/.dotfiles/khd/.khdrc                   ~/.khdrc                     
+ln -s ~/.dotfiles/.tmux.conf.mac               ~/.tmux.conf                 
+ln -s ~/.dotfiles/com.googlecode.iterm2.plis   ~/com.googlecode.iterm2.plist
+ln -s ~/.dotfiles/                             ~/.hammaerspoon              
