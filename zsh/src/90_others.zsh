4eef9f91106c0a24e6e45f55297af41709cc3a72
yuki.osako <yuki.osako@gree.net>
2021-10-18 16:35:24 +0900

    Remove auto sts function from others.zsh

--- zsh/src/90_others.zsh
+++ zsh/src/90_others.zsh
@@ -8,27 +8,6 @@ PERIOD=30
 add-zsh-hook periodic show-time
 
 
-function auto-sts() {
-    if [[ -z "$AWS_PRODUCT" ]]; then
-         unset AWS_SECURITY_TOKEN
-    else
-      if [[ -z "$STS_EXPIRATION_UNIXTIME" ]]; then
-          echo "\ngetting sts token...\n"
-          export _STS_CURRENT_AWS_PRODUCT=$AWS_PRODUCT
-          eval `stsenv`
-      elif [[ "$_STS_CURRENT_AWS_PRODUCT" != "$AWS_PRODUCT" ]]; then
-          echo "\nupdating sts token for account switching...\n"
-          export _STS_CURRENT_AWS_PRODUCT=$AWS_PRODUCT
-          eval `stsenv`
-      elif [[ "$STS_EXPIRATION_UNIXTIME" -lt `date +%s` ]]; then
-          echo "\nupdating sts token...\n"
-          # export _STS_CURRENT_AWS_PRODUCT=$AWS_PRODUCT
-          eval `stsenv`
-      fi
-    fi
-}
-
-
 function _precmd() {
     auto-sts
     update-prompt
