3f342d514cc7d4a3dbb6ac3e0a33bd259eed8bbe
yuzu <yuzu6120@gmail.com>
2021-11-21 14:26:47 +0900

    Update gcloud zsh completion loader

--- zsh/src/completion/_gcloud.zsh
+++ zsh/src/completion/_gcloud.zsh
@@ -1,1 +1,16 @@
-source /usr/local/Caskroom/google-cloud-sdk/latest/google-cloud-sdk/completion.zsh.inc
+function source-if-exists {
+  file=$1
+  if [ -f "$file" ]; then
+    source "$file"
+  else
+    echo "$file is not found. skip."
+  fi
+}
+
+source-if-exists '/usr/local/Caskroom/google-cloud-sdk/latest/google-cloud-sdk/completion.zsh.inc'
+
+# update PATH for the Google Cloud SDK.
+source-if-exists "$HOME/google-cloud-sdk/path.zsh.inc"
+# enable shell command completion for gcloud.
+source-if-exists "$HOME/google-cloud-sdk/completion.zsh.inc"
+
