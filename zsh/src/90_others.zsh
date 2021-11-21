a443afb546b4ac2d734d9853443eea7bf7e3a058
yuzu <yuzu6120@gmail.com>
2021-11-21 14:27:20 +0900

    Update zsh precmd for env without auto-sts

--- zsh/src/90_others.zsh
+++ zsh/src/90_others.zsh
@@ -1,18 +1,15 @@
 ## period
 function show-time()
 {
-    echo ""
-    LC_TIME=c date --iso-8601=minutes | sed -e 's/T/ /g' -e 's/^(.*)$/($1)/'
+  echo ""
+  LC_TIME=c date --iso-8601=minutes | sed -e 's/T/ /g' -e 's/^(.*)$/($1)/'
 }
 PERIOD=30
 add-zsh-hook periodic show-time
 
 
 function _precmd() {
-    auto-sts
-    update-prompt
+  (which auto-sts > /dev/null) && auto-sts
+  update-prompt
 }
 add-zsh-hook precmd _precmd
-
-
-
