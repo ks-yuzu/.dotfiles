336c857c6a6f9f65cc663f668659cd3f75689ec1
yuzu <yuzu6120@gmail.com>
2022-03-10 22:32:00 +0900

    Update git functions for WSL2 (and refactor)

--- zsh/src/32_git.zsh
+++ zsh/src/32_git.zsh
@@ -1,12 +1,9 @@
 ## git status
 function __git-status()
 {
-    # if [ "$(git rev-parse --is-inside-work-tree 2> /dev/null)" = 'true' ]; then
-        echo git status -sb
-        git status -sb
-        echo '\n'
-    # fi
-    zle reset-prompt
+    BUFFER='git status -sb'
+    echo $BUFFER && zle accept-line
+    echo '\n' && zle reset-prompt
 }
 zle -N __git-status && bindkey "^gs" $_
 
@@ -14,28 +11,24 @@ zle -N __git-status && bindkey "^gs" $_
 ## git diff
 function __peco-git-diff()
 {
-    local O_PWD=$(pwd)
-    local GIT_ROOT=$(perl -e "print '$O_PWD' . '/..' x $(get-git-dir-depth)")
-
-    cd $GIT_ROOT 2> /dev/null
+    #local GIT_ROOT="$(pwd)$(perl -e "print '/..' x $(get-git-dir-depth)")"
+    local GIT_ROOT="$(perl -e "print '../' x $(get-git-dir-depth)")"
 
     if [ $(git status --porcelain | wc -l) -eq 0 ]; then
-        echo $(git status | perl -pe 's/On branch (.*)/[branch: $1] /')
-        return
-    fi
-
-    local SELECTED_FILE_TO_DIFF="$(git status --porcelain | \
-                                  grep '^ *M' | \
-                                  peco --query "$LBUFFER" | \
-                                  awk -F ' ' '{print $NF}')"
-    if [ -n "$SELECTED_FILE_TO_DIFF" ]; then
-        git diff $(echo "$SELECTED_FILE_TO_DIFF" | tr '\n' ' ')
+      echo ''
+      git status
     else
-        echo 'Not found the file.'
+      local SELECTED_FILE="$(git status --porcelain | \
+                               grep '^ *M' | \
+                               peco --query "$LBUFFER" | \
+                               awk -F ' ' '{print $NF}')"
+      [ -n "$SELECTED_FILE" ] || return
+
+      BUFFER="(builtin cd '$GIT_ROOT' && git diff $(echo "$SELECTED_FILE" | tr '\n' ' '))"
+      echo $BUFFER && zle accept-line
     fi
 
-    cd $O_PWD > /dev/null
-    zle reset-prompt
+    echo '\n' && zle reset-prompt
 }
 zle -N __peco-git-diff && bindkey "^gd" $_
 
@@ -43,15 +36,14 @@ zle -N __peco-git-diff && bindkey "^gd" $_
 ## git add
 function __peco-git-add()
 {
-    local SELECTED_FILE_TO_ADD="$(git status --short | \
-                                  peco --query "$LBUFFER" | \
-                                  awk -F ' ' '{print $NF}')"
-    if [ -n "$SELECTED_FILE_TO_ADD" ]; then
-      BUFFER="git add $(echo "$SELECTED_FILE_TO_ADD" | tr '\n' ' ')"
-      CURSOR=$#BUFFER
-    fi
-    zle accept-line
-    zle reset-prompt
+    local SELECTED_FILE="$(git status --short | \
+                             peco --query "$LBUFFER" | \
+                             awk -F ' ' '{print $NF}')"
+    [ -n "$SELECTED_FILE" ] || return
+
+    BUFFER="git add $(echo "$SELECTED_FILE" | tr '\n' ' ')"
+    echo $BUFFER && zle accept-line
+    echo '\n' && zle reset-prompt
 }
 zle -N __peco-git-add && bindkey "^ga" $_
 
@@ -59,8 +51,9 @@ zle -N __peco-git-add && bindkey "^ga" $_
 ## git checkout
 function __peco-git-checkout()
 {
-    git checkout $(git branch -a | peco | sed -e 's/^..//g' -e '/->/d' | awk '!a[$0]++')
-    zle reset-prompt
+    BUFFER="git checkout $(git branch -a | peco | sed -e 's/^..//g' -e '/->/d' | awk '!a[$0]++')"
+    echo $BUFFER && zle accept-line
+    echo '\n' && zle reset-prompt
 }
 zle -N __peco-git-checkout && bindkey "^go" $_
 
@@ -68,8 +61,9 @@ zle -N __peco-git-checkout && bindkey "^go" $_
 ## git graph
 function __git-graph()
 {
-    git log --graph --all --pretty=format:'%C(green)%cd%C(reset) %C(red)%h%C(reset) %C(yellow bold)%d%C(reset) %C(bold)%s%C(reset) %C(blue bold)<%an>%C(reset)' --abbrev-commit --date=format:'%Y-%m-%d %H:%M'
-    zle reset-prompt
+    BUFFER="git log --graph --all --pretty=format:'%C(green)%cd%C(reset) %C(red)%h%C(reset) %C(yellow bold)%d%C(reset) %C(bold)%s%C(reset) %C(blue bold)<%an>%C(reset)' --abbrev-commit --date=format:'%Y-%m-%d %H:%M'"
+    echo $BUFFER && zle accept-line
+    echo '\n' && zle reset-prompt
 }
 zle -N __git-graph && bindkey "^gg" $_
 
@@ -77,35 +71,37 @@ zle -N __git-graph && bindkey "^gg" $_
 ## git graph rich
 function __git-graph-rich()
 {
-    git log --graph --all --pretty=format:'%C(red reverse)%d%Creset%C(white reverse) %h% Creset %C(green reverse) %an %Creset %C(cyan bold)%ad (%ar)%Creset%n%C(white bold)%w(80)%s%Creset%n%n%w(80,2,2)%b' --abbrev-commit --date=format:'%Y-%m-%d %H:%M:%S' --name-status
-    zle reset-prompt
+    BUFFER="git log --graph --all --pretty=format:'%C(red reverse)%d%Creset%C(white reverse) %h% Creset %C(green reverse) %an %Creset %C(cyan bold)%ad (%ar)%Creset%n%C(white bold)%w(80)%s%Creset%n%n%w(80,2,2)%b' --abbrev-commit --date=format:'%Y-%m-%d %H:%M:%S' --name-status"
+    echo $BUFFER && zle accept-line
+    echo '\n' && zle reset-prompt
 }
 zle -N __git-graph-rich && bindkey "^g^g" $_
 
 
-## git commit -v
+## set command 'git commit'
 function __git-commit()
 {
-    # git commit -v
-    # zle reset-prompt
     BUFFER="git commit -m ''"
-    CURSOR=15
-
+    CURSOR=$(($#BUFFER - 1))
 }
 zle -N __git-commit && bindkey "^gc" $_
 
 
+## utils
 function __cd-git-root
 {
     if [[ $(get-git-dir-depth) != '0' ]]; then
-        cd $(get-git-root-dir)
+        builtin cd $(get-git-root-dir)
     fi
     zle reset-prompt
 }
 zle -N __cd-git-root && bindkey "^gr" $_
 
+function get-path-from-git-root {
+  git rev-parse --show-prefix 2> /dev/null
+}
 
-function get-git-root-dir()
+function get-git-root-dir
 {
     if [[ $(get-git-dir-depth) != '0' ]]; then
         local REL_PATH_FROM_GIT_ROOT=$(get-path-from-git-root | perl -pe 's!/$!!')
@@ -113,10 +109,9 @@ function get-git-root-dir()
     fi
 }
 
-
 function get-git-dir-depth
 {
-    local depth=$(get-path-from-git-root | head -n 1 | perl -ne 'print s!/!!g')
+    local depth=$(get-path-from-git-root | head -n1 | perl -ne 'print s!/!!g')
     if [[ $depth == '' ]];then
         echo '0'
     else
