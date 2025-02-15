2ffb0e58063fe384c7b515b24f9a95fb9a3ca074
yuzu <yuzu6120@gmail.com>
2025-02-15 15:51:33 +0900

    Update zsh hooks

--- zsh/src/90_hooks.zsh
+++ zsh/src/90_hooks.zsh
@@ -2,7 +2,7 @@
 function show-time()
 {
   echo ""
-  LC_TIME=c date --iso-8601=minutes | sed -e 's/T/ /g' -e 's/^(.*)$/($1)/'
+  LC_TIME=c date --iso-8601=seconds | sed -e 's/T/ /g' -e 's/+09:00/ (JST)/'
 }
 PERIOD=30
 add-zsh-hook periodic show-time
@@ -10,6 +10,8 @@ add-zsh-hook periodic show-time
 
 function _precmd() {
   (which auto-sts > /dev/null) && auto-sts
+
+  RPROMPT='$(rprompt)'
   update-prompt
 }
 add-zsh-hook precmd _precmd
@@ -21,3 +23,10 @@ function _preexec() {
       || argocd login $(argocd context | sed -n '2,$p' | cut -c 4- | peco | awk '{print $1}') --grpc-web --sso
   fi
 }
+
+function on-accept-line() {
+  RPROMPT='[%D{%Y/%m/%d %H:%M:%S}]'
+  zle .reset-prompt
+  zle .accept-line
+}
+zle -N accept-line on-accept-line
