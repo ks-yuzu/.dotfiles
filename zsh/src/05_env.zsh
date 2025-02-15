4195ad23e23d6ce9149a7744f5cffc8bb9648df2
yuzu <yuzu6120@gmail.com>
2025-02-15 15:41:59 +0900

    Update env.zsh

--- zsh/src/05_env.zsh
+++ zsh/src/05_env.zsh
@@ -1,16 +1,18 @@
+export LC_TIME='C'
+export TIMEFMT=$'\n\n========================\nCommand: %J\nCPU:     %P\nuser:    %*Us\nsystem:  %*Ss\ntotal:   %*Es\n========================\n'
+
 export LYNX_CFG=~/.lynx
 #export EDITOR='emacsclient -t'
 export CVSEDITOR="${EDITOR}"
 export SVN_EDITOR="${EDITOR}"
 export GIT_EDITOR="${EDITOR}"
 
-# less command
 #export VISUAL='emacsclient -t'
-export LESS='-N -M -R'
+export LESS='-N -M -R -s +Gg'
+export BAT_PAGER="less -n"
 
 export FZF_DEFAULT_OPTS="--cycle --no-mouse --reverse --prompt='QUERY> ' --color=16"
 
-export LC_TIME='C'
-
 export AWS_DEFAULT_REGION=ap-northeast-1
 export KUBECTL_EXTERNAL_DIFF='colordiff -u'
+export USE_GKE_GCLOUD_AUTH_PLUGIN=True
